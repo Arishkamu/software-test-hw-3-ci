b7c999de421d06b314ea5c77d3db802274a306c2
Arina Ivanova <arishkamu@gmail.com>
2025-03-25 11:14:41 +0300

    copy hw repo

---
+++ src/test/java/org/itmo/testing/lab2/integration/UserAnalyticsIntegrationTest.java
@@ -0,0 +1,351 @@
+package org.itmo.testing.lab2.integration;
+
+import io.javalin.Javalin;
+import io.restassured.RestAssured;
+import org.itmo.testing.lab2.controller.UserAnalyticsController;
+import org.junit.jupiter.api.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class UserAnalyticsIntegrationTest {
+
+    private Javalin app;
+    private final LocalDateTime now = LocalDateTime.now();
+    private final String monthNow = YearMonth.now().toString();
+
+    @BeforeAll
+    void setUp() {
+        app = UserAnalyticsController.createApp();
+        int port = 1234;
+        app.start(port);
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @AfterAll
+    void tearDown() {
+        app.stop();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Тест регистрации пользователя")
+    void testUserRegistration() {
+        given()
+                .queryParam("userId", "user1")
+                .queryParam("userName", "Alice")
+                .when()
+                .post("/register")
+                .then()
+                .statusCode(200)
+                .body(equalTo("User registered: true"));
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Тест регистрации пользователя без id")
+    void testUserRegistrationNoId() {
+        given()
+                .queryParam("userName", "Alice")
+                .when()
+                .post("/register")
+                .then()
+                .statusCode(400)
+                .body(equalTo("Missing parameters"));
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Тест регистрации пользователя без имени")
+    void testUserRegistrationNoName() {
+        given()
+                .queryParam("userId", "user2")
+                .when()
+                .post("/register")
+                .then()
+                .statusCode(400)
+                .body(equalTo("Missing parameters"));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Тест регистрации пользователя без имени")
+    void testUserRegistrationExtraParam() {
+        given()
+                .queryParam("userId", "user2")
+                .queryParam("userName", "Bob")
+                .queryParam("extra", "extra")
+                .when()
+                .post("/register")
+                .then()
+                .statusCode(200)
+                .body(equalTo("User registered: true"));
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Тест регистрации пользователя с пустым id")
+    void testUserRegistrationEmptyId() {
+        given()
+                .queryParam("userId", "")
+                .queryParam("userName", "Cecil")
+                .when()
+                .post("/register")
+                .then()
+                .statusCode(200)
+                .body(equalTo("User registered: true"));
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Тест записи сессии")
+    void testRecordSession() {
+        given()
+                .queryParam("userId", "user1")
+                .queryParam("loginTime", now.minusHours(1).toString())
+                .queryParam("logoutTime", now.toString())
+                .when()
+                .post("/recordSession")
+                .then()
+                .statusCode(200)
+                .body(equalTo("Session recorded"));
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Тест записи сессии без id")
+    void testRecordSessionNoId() {
+        given()
+                .queryParam("loginTime", now.minusHours(1).toString())
+                .queryParam("logoutTime", now.toString())
+                .when()
+                .post("/recordSession")
+                .then()
+                .statusCode(400)
+                .body(equalTo("Missing parameters"));
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Тест записи сессии без loginTime")
+    void testRecordSessionNoLoginTime() {
+        given()
+                .queryParam("userId", "user1")
+                .queryParam("logoutTime", now.toString())
+                .when()
+                .post("/recordSession")
+                .then()
+                .statusCode(400)
+                .body(equalTo("Missing parameters"));
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Тест записи сессии без logoutTime")
+    void testRecordSessionNoLogoutTim() {
+        given()
+                .queryParam("userId", "user1")
+                .queryParam("loginTime", now.minusHours(1).toString())
+                .when()
+                .post("/recordSession")
+                .then()
+                .statusCode(400)
+                .body(equalTo("Missing parameters"));
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Тест записи сессии с некорректным временем")
+    void testRecordSessionInvalidTime() {
+        given()
+                .queryParam("userId", "user1")
+                .queryParam("loginTime", "2025-03-04")
+                .queryParam("logoutTime", now.minusHours(1).toString())
+                .when()
+                .post("/recordSession")
+                .then()
+                .statusCode(400)
+                .body(equalTo("Invalid data: Text '2025-03-04' could not be parsed at index 10"));
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("Тест записи сессии login>logout")
+    void testRecordSessionMinusTime() {
+        given()
+                .queryParam("userId", "user1")
+                .queryParam("loginTime", now.toString())
+                .queryParam("logoutTime", now.minusHours(2).toString())
+                .when()
+                .post("/recordSession")
+                .then()
+                .statusCode(200)
+                .body(equalTo("Session recorded"));
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("Тест получения общего времени активности")
+    void testGetTotalActivity() {
+        given()
+                .queryParam("userId", "user1")
+                .when()
+                .get("/totalActivity")
+                .then()
+                .statusCode(200)
+                .body(containsString("Total activity:"))
+                .body(containsString("minutes"));
+    }
+
+    @Test
+    @Order(8)
+    @DisplayName("Тест получения общего времени активности с несуществующим пользователем")
+    void testGetTotalActivityUserNotExist() {
+        given()
+                .queryParam("userId", "notExist")
+                .when()
+                .get("/totalActivity")
+                .then()
+                .statusCode(400)
+                .body(equalTo("Invalid data: No sessions found for user"));
+    }
+
+    @Test
+    @Order(8)
+    @DisplayName("Тест получения общего времени активности без записей")
+    void testGetTotalActivityNoSessions() {
+        given()
+                .queryParam("userId", "user2")
+                .when()
+                .get("/totalActivity")
+                .then()
+                .statusCode(400)
+                .body(equalTo("Invalid data: No sessions found for user"));
+    }
+
+    @Test
+    @Order(8)
+    @DisplayName("Тест получения общего времени без id")
+    void testGetTotalActivityNoId() {
+        given()
+                .queryParam("userIds", "users")
+                .when()
+                .get("/totalActivity")
+                .then()
+                .statusCode(400)
+                .body(equalTo("Missing userId"));
+    }
+
+    @Test
+    @Order(9)
+    @DisplayName("Тест получения неактивных пользователей")
+    void testGetInactiveUsers() {
+        given()
+                .queryParam("days", 10)
+                .when()
+                .get("/inactiveUsers")
+                .then()
+                .statusCode(200)
+                .body(equalTo("[]"));
+    }
+
+    @Test
+    @Order(10)
+    @DisplayName("Тест получения неактивных пользователей без days")
+    void testGetInactiveUsersNoDays() {
+        given()
+                .queryParam("DAYS", 10)
+                .when()
+                .get("/inactiveUsers")
+                .then()
+                .statusCode(400)
+                .body(equalTo("Missing days parameter"));
+    }
+
+    @Test
+    @Order(10)
+    @DisplayName("Тест получения неактивных пользователей неверная дата")
+    void testGetInactiveUsersInvalidDays() {
+        given()
+                .queryParam("days", 5.)
+                .when()
+                .get("/inactiveUsers")
+                .then()
+                .statusCode(400)
+                .body(equalTo("Invalid number format for days"));
+    }
+
+    @Test
+    @Order(11)
+    @DisplayName("Тест получения неактивных пользователей отрицательная дата")
+    void testGetInactiveUsersMinusDays() {
+        given()
+                .queryParam("days", -10)
+                .when()
+                .get("/inactiveUsers")
+                .then()
+                .statusCode(200)
+                .body(equalTo("[\"user1\"]"));
+    }
+
+    @Test
+    @Order(12)
+    @DisplayName("Тест получения статистики за месяц")
+    void testGetMonthlyActivity() {
+        given()
+                .queryParam("userId", "user1")
+                .queryParam("month", monthNow)
+                .when()
+                .get("/monthlyActivity")
+                .then()
+                .statusCode(200)
+                .body(not(equalTo("{}")));
+    }
+
+    @Test
+    @Order(13)
+    @DisplayName("Тест получения статистики за месяц без id")
+    void testGetMonthlyActivityNoId() {
+        given()
+                .queryParam("extraParam", "extra")
+                .queryParam("month", monthNow)
+                .when()
+                .get("/monthlyActivity")
+                .then()
+                .statusCode(400)
+                .body(equalTo("Missing parameters"));
+    }
+
+    @Test
+    @Order(13)
+    @DisplayName("Тест получения статистики за месяц без month")
+    void testGetMonthlyActivityNoMonth() {
+        given()
+                .queryParam("userId", "user1")
+                .queryParam("months", monthNow)
+                .when()
+                .get("/monthlyActivity")
+                .then()
+                .statusCode(400)
+                .body(equalTo("Missing parameters"));
+    }
+
+    @Test
+    @Order(13)
+    @DisplayName("Тест получения статистики за месяц некорректный month")
+    void testGetMonthlyActivityInvalidMonth() {
+        given()
+                .queryParam("userId", "user1")
+                .queryParam("month", "12-2025")
+                .when()
+                .get("/monthlyActivity")
+                .then()
+                .statusCode(400)
+                .body(equalTo("Invalid data: Text '12-2025' could not be parsed at index 0"));
+    }
+}
---
+++ src/test/java/org/itmo/testing/lab2/service/UserStatusServiceTest.java
@@ -0,0 +1,94 @@
+package org.itmo.testing.lab2.service;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UserStatusServiceTest {
+
+    private UserAnalyticsService userAnalyticsService;
+    private UserStatusService userStatusService;
+
+    @BeforeAll
+    void setUp() {
+        userAnalyticsService = mock(UserAnalyticsService.class);
+        userStatusService = new UserStatusService(userAnalyticsService);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, -10, Inactive",
+            "2, 0, Inactive",
+            "3, 50, Inactive",
+            "4, 100, Active",
+            "5, 150, Highly active"
+    })
+    public void testGetUserStatus_Active(int n, long activityTime, String expectedStatus) {
+        // Настроим поведение mock-объекта
+        when(userAnalyticsService.getTotalActivityTime("user123")).thenReturn(activityTime);
+
+        String status = userStatusService.getUserStatus("user123");
+
+        assertEquals(expectedStatus, status);
+
+        verify(userAnalyticsService, times(n)).getTotalActivityTime("user123");
+    }
+
+    @Test
+    public void getUserLastSessionDate() {
+        LocalDateTime login = LocalDateTime.now().minusHours(1);
+        LocalDateTime logout = LocalDateTime.now();
+        List<UserAnalyticsService.Session> sessions = List.of(
+                new UserAnalyticsService.Session(login.plusDays(1), logout.plusDays(1)),
+                new UserAnalyticsService.Session(login.plusDays(10), logout.plusDays(10)),
+                new UserAnalyticsService.Session(login, logout)
+        );
+
+        when(userAnalyticsService.getUserSessions("user123")).thenReturn(sessions);
+
+        Optional<String> lastSession = userStatusService.getUserLastSessionDate("user123");
+
+        assertTrue(lastSession.isPresent());
+        assertEquals(logout.toLocalDate().toString(), lastSession.get());
+
+        verify(userAnalyticsService, times(2)).getUserSessions("user123");
+    }
+
+    @Test
+    public void getUserLastSessionDateNoSessions() {
+        when(userAnalyticsService.getUserSessions("user123")).thenReturn(List.of());
+
+        NoSuchElementException exc = assertThrows(NoSuchElementException.class,
+                () -> userStatusService.getUserLastSessionDate("user123"));
+        assertNull(exc.getMessage());
+    }
+
+    @Disabled
+    public void getUserLastSessionDateNotCorrectOrder() {
+        LocalDateTime login = LocalDateTime.now().minusHours(1);
+        LocalDateTime logout = LocalDateTime.now();
+        List<UserAnalyticsService.Session> sessions = List.of(
+                new UserAnalyticsService.Session(login, logout),
+                new UserAnalyticsService.Session(login.plusDays(1), logout.plusDays(1)),
+                new UserAnalyticsService.Session(login.plusDays(10), logout.plusDays(10))
+        );
+
+        when(userAnalyticsService.getUserSessions("user123")).thenReturn(sessions);
+
+        Optional<String> lastSession = userStatusService.getUserLastSessionDate("user123");
+
+        assertTrue(lastSession.isPresent());
+        assertEquals(logout.toLocalDate().toString(), lastSession.get());
+
+        verify(userAnalyticsService).getUserSessions("user123");
+    }
+}
